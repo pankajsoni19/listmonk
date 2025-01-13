f17a785c9b5f63d6c9000db81a599f86f7b0e008
Pankaj Soni <pankajsoni19@live.com>
2025-01-13 16:42:29 +0530

    update runtype

--- queries.sql
+++ queries.sql
@@ -542,7 +542,9 @@ SELECT  c.id, c.uuid, c.name, c.subject, c.from_email,
         c.messenger, c.started_at, c.to_send, c.sent, c.type,
         c.body, c.altbody, c.send_at, c.headers, c.status, c.content_type, c.tags,
         c.template_id, c.archive, c.archive_slug, c.archive_template_id, c.archive_meta,
-        c.created_at, c.updated_at, sliding_window, sliding_window_rate, sliding_window_duration,
+        c.created_at, c.updated_at, 
+        c.sliding_window, c.sliding_window_rate, c.sliding_window_duration,
+        c.run_type,
         COUNT(*) OVER () AS total,
         (
             SELECT COALESCE(ARRAY_TO_JSON(ARRAY_AGG(l)), '[]') FROM (
